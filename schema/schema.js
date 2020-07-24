191b045e45c4dc1e28fbd8bfef4547fec79acf8b
d930379 <you@example.com>
2020-07-24 16:17:57 +0530

    react graphql

---
+++ schema/schema.js
@@ -0,0 +1,38 @@
+const graphql = require("graphql")
+const { GraphQLObjectType,GraphQLString,GraphQLInt,GraphQLSchema } = graphql;
+
+//const _ = require("lodash")
+const axios = require("axios")
+
+
+const UserType=new GraphQLObjectType({
+    name:'User',
+    fields:{
+        id:{ type: GraphQLString},
+        firstName:{ type:GraphQLString},
+        age:{type:GraphQLInt}
+    }
+})
+
+// const users = [
+//     {id:'23',firstName:"Nirmesh",age:23},
+//     {id:47,firstName:"Selva",age:53},
+// ]
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:{
+        user:{
+            type:UserType,
+            args:{id:{type:GraphQLString}},
+            resolve(parentValue,args){
+               // return _.find(users,{id:args.id})
+               return axios.get(`http://localhost:3000/users/${args.id}`).then(resp=>resp.data)
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+})
---
+++ server.js
@@ -0,0 +1,17 @@
+const express=require("express")
+//const expressGraphQL = require("express-graphql");
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema')
+
+const app = express();
+
+
+app.use("/graphql",graphqlHTTP({
+    graphiql:true,
+    schema
+}))
+
+
+app.listen(4000,()=>{
+  console.log("started on 4000");  
+})
